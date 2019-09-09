8a9e60d5fa338dc6db720ae00913bfa3a3e7c3a2
kyeongmin0022 <kyeongmin.kang@gluwa.com>
2019-09-09 12:21:00 +0900

    chapter4 들어가는지 확인

---
+++ Chapter4/Chapter4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    public class IntStack
+    {
+        private int max;
+        private int ptr;
+        private int[] stk;
+
+        public class EmptyIntStackException : Exception
+        {
+            public EmptyIntStackException() { }
+
+        }
+
+        public class OverflowIntStackExcption : Exception
+        {
+            public OverflowIntStackExcption() { }
+        }
+
+        public IntStack(int capacity)
+        {
+            ptr = 0;
+            max = capacity;
+            try
+            {
+                stk = new int[max];
+
+            }
+            catch (OutOfMemoryException e)
+            {
+                max = 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
