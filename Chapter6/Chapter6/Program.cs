665d53766818c288f367ef70e1260adad6034cc7
kyeongmin0022 <kyeongmin.kang@gluwa.com>
2019-09-11 17:17:21 +0900

    정렬시작

---
+++ Chapter6/Chapter6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6
+{
+    class Program
+    {
+        static void swap(int[] a, int idx1, int idx2)
+        {
+            int t = a[idx1];
+            a[idx1] = a[idx2];
+            a[idx2] = t;
+        }
+
+        static void bubbleSort(int[] a, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    if(a[j-1] > a[j])
+                    {
+                        swap(a, j - 1, j);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("버블 정렬(버전 1) : ");
+          
+        }
+    }
+}
