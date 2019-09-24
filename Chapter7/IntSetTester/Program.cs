85e9ed466431bb889ca609c7b2b53858b50f8c8b
kyeongmin0022 <kyeongmin.kang@gluwa.com>
2019-09-24 17:23:51 +0900

    집합 끝끝

---
+++ Chapter7/IntSetTester/Program.cs
@@ -0,0 +1,53 @@
+﻿using Chapter7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IntSetTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntSet s1 = new IntSet(20);
+            IntSet s2 = new IntSet(20);
+            IntSet s3 = new IntSet(20);
+
+            s1.add(10);
+            s1.add(15);
+            s1.add(20);
+            s1.add(25);
+
+            s1.copyTo(s2);
+            s2.add(12);
+            s2.remove(25);
+
+            s3.copyFrom(s2);
+
+            Console.WriteLine("s1 = " + s1.toString());
+            Console.WriteLine("s2 = " + s2.toString());
+            Console.WriteLine("s3 = " + s3.toString());
+
+            Console.WriteLine("집합 s1에 15는 " + (s1.contains(15) ? "포함됩니다." : "포함되지 않습니다."));
+
+            Console.WriteLine("집합 s2에 25는 " + (s2.contains(25) ? "포함됩니다." : "포함되지 않습니다."));
+
+            Console.WriteLine("집합 s1과 s2는 " + (s1.equalTo(s2) ? "같습니다." : "같지 않습니다."));
+
+            Console.WriteLine("집합 s2과 s3는 " + (s2.equalTo(s3) ? "같습니다." : "같지 않습니다."));
+
+            s3.unionOf(s1, s2);
+
+            Console.WriteLine("집합 s1과 s2의 합집합은 " + s3.toString() + "입니다.");
+
+            Console.WriteLine(s1.isFull());
+            s1.clear();
+            Console.WriteLine(s1.isEmpty());
+            Console.WriteLine(s1.toString());
+
+        }
+    }
+}
---
+++ Chapter7/IntsortedSet_5/Program.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntsortedSet_5
+{
+    public class IntSet
+    {
+        private int max; // 집합 최대 개수
+        private int num; // 집합 요소 개수
+        private int[] set; // 집합 본체
+
+        public IntSet(int capacity)
+        {
+            num = 0;
+            max = capacity;
+            try
+            {
+                set = new int[max]; //집합 배열 생성
+            }
+            catch (OutOfMemoryException) // 실패
+            {
+                max = 0;
+            }
+        }
+
+        // 찾지 못한 경우 (-삽입포인트-1) 반환
+        public int IntSortedSet(int n)
+        {
+            int pl = 0;
+            int pr = 0;
+            do
+            {
+                int pc = (pl + pr) / 2;
+                if (set[pc] == n)
+                    return pc;
+                else if (set[pc] < n)
+                    pl = pc + 1;
+                else
+                    pr = pc - 1;
+            } while (pr <= pl);
+
+            return -pl - 1;
+        }
+
+        public int capacity()
+        {
+            return max;
+        }
+
+        public int size()
+        {
+            return num;
+        }
+
+        public int indexOf(int n)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                if (set[i] == n)
+                    return i;
+            }
+            return -1;
+        }
+
+        public bool contains(int n)
+        {
+            return (indexOf(n) != -1) ? true : false;
+        }
+
+        public bool add(int n)
+        {
+            // 가득 찼거나 이미 n이 존재할 때
+            if (num >= max || contains(n) == true)
+            {
+                return false;
+            }
+            else
+            {
+                set[num++] = n; // 가장 마지막에 추가
+                return true;
+            }
+        }
+
+        public bool remove(int n)
+        {
+            int idx;
+
+            if (num <= 0 || (idx = indexOf(n)) == -1) // 비어 있거나 n이 존재안함
+            {
+                return false;
+            }
+            else
+            {
+                set[idx] = set[--num]; //마지막 요소를 삭제한 곳으로 옮김
+                return true;
+            }
+        }
+
+        // 집합 s에 복사
+        public void copyTo(IntSet s)
+        {
+            int n = (s.max < num) ? s.max : num;
+            for (int i = 0; i < n; i++)
+            {
+                s.set[i] = set[i];
+            }
+            s.num = n;
+
+        }
+
+        // 집한 s를 복사
+        public void copyFrom(IntSet s)
+        {
+            int n = (max < s.num) ? max : s.num;
+            for (int i = 0; i < n; i++)
+            {
+                set[i] = s.set[i];
+            }
+            num = n;
+        }
+
+        public bool equalTo(IntSet s)
+        {
+            if (num != s.num)
+                return false;
+
+            for (int i = 0; i < num; i++)
+            {
+                int j = 0;
+                for (; j < s.num; j++)
+                {
+                    if (set[i] == s.set[j])
+                        break;
+                }
+                if (j == s.num)
+                    return false;
+
+            }
+            return true;
+        }
+
+        // 집한 s1과 s2 합집합 복사
+        public void unionOf(IntSet s1, IntSet s2)
+        {
+            copyFrom(s1); //집합 s1복사
+            for (int i = 0; i < s2.num; i++)
+            {
+                add(s2.set[i]);
+            }
+        }
+
+        // 7-1
+        // 공집합인지 확인
+        public bool isEmpty()
+        {
+            return num == 0;
+
+        }
+
+        // 집합이 가득 찼는지 확인
+        public bool isFull()
+        {
+            return num >= max;
+        }
+
+        // 집합을 초기화
+        public void clear()
+        {
+            num = 0;
+        }
+
+        // 7-2
+        // 집합 s의 모든 요소를 추가할 때 집합이 변경되는가?
+        public bool add(IntSet s)
+        {
+            bool flag = false;
+            for (int i = 0; i < num; i++)
+            {
+                if (add(set[i]) == true)
+                    return true;
+
+            }
+            return flag;
+        }
+
+        // 집합 s에 들어있는 요소만 남기고 들어있지 않은 요소는 삭제할 때, 집합이 변경되는가?
+        public bool retain(IntSet s)
+        {
+            bool flag = false;
+            for (int i = 0; i < num; i++)
+            {
+                if (s.contains(set[i]) == false)
+                {
+                    remove(set[i]);
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+        // 집합 s에 들어있는 요소를 삭제하는 메서드
+        public bool remove(IntSet s)
+        {
+            bool flag = false;
+            for (int i = 0; i < num; i++)
+            {
+                if (s.contains(set[i]) == true)
+                {
+                    remove(set[i]);
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+        // 7-3
+        // 집합 s의 부분집합인지 확인
+        public bool isSubsetOf(IntSet s)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                int j = 0;
+                for (; j < s.num; j++)
+                {
+                    if (set[i] == s.set[j])
+                        break;
+                }
+                if (j == s.num)
+                    return false;
+            }
+            return true;
+
+        }
+
+        // 집합 s의 진부분집합인지 확인
+        public bool isProperSubsetOf(IntSet s)
+        {
+            if (num >= s.num)
+                return false;
+            return s.isSubsetOf(s);
+        }
+
+        //7-4
+        // 집합 s1과 집합 s2의 교집합을 복사하는 메서드
+
+        public void intersectionOf(IntSet s1, IntSet s2)
+        {
+            for (int i = 0; i < s1.num; i++)
+            {
+                if (s2.contains(s1.set[i]))
+                    add(s1.set[i]);
+            }
+
+        }
+
+
+
+        // 집합 s1과 집합 s2의 교집합을 복사하는 메서드
+        public void differenceOf(IntSet s1, IntSet s2)
+        {
+            for (int i = 0; i < s1.num; i++)
+            {
+                if (!s2.contains(s1.set[i]))
+                    add(s1.set[i]);
+            }
+        }
+
+
+        public string toString()
+        {
+            StringBuilder temp = new StringBuilder("{ ");
+            for (int i = 0; i < num; i++)
+            {
+                temp.Append(set[i] + " ");
+            }
+            temp.Append(" }");
+            return temp.ToString();
+
+        }
+    }
+}
