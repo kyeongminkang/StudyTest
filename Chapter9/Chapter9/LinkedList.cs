3c9f45ac1353244c3ea6ca5525c443a9e7d537e4
kyeongmin0022 <kyeongmin.kang@gluwa.com>
2019-09-26 18:35:01 +0900

    linkedlist 이해 하기전에 문법 때문에 코드 못 써가지고 삽질중..

---
+++ Chapter9/Chapter9/LinkedList.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Chapter9
+{
+    public class LinkedList<E>
+    {
+        public class Node<E>
+        {
+            public E data;
+            public Node<E> next;
+
+            public Node(E data, Node<E> next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+        }
+
+        private Node<E> head;
+        private Node<E> crnt;
+
+        public LinkedList()
+        {
+            head = crnt = null;
+        }
+
+        public E search(E obj, Comparer<E> c)
+        {
+            Node<E> ptr = head;
+
+            while(ptr != null)
+            {
+              if(c.Compare(obj, ptr.data) == 0)
+                {
+                    crnt = ptr;
+                    return ptr.data;
+                }
+                ptr = ptr.next;
+            }
+            return default(E);
+            // 원래는 null이 나와야 하는데....
+        }
+
+        public void addFirst(E obj)
+        {
+            Node<E> ptr = head;
+            head = crnt = new Node<E>(obj, ptr);
+        }
+
+        public void removeFirst()
+        {
+            if (head != null)
+                head = crnt = head.next;
+        }
+
+        public void removeLast()
+        {
+            if (head != null)
+            {
+                if (head.next == null)
+                    removeFirst();
+                else
+                {
+                    Node<E> ptr = head;
+                    Node<E> pre = head;
+
+                    while (ptr.next != null)
+                    {
+                        pre = ptr;
+                        ptr = ptr.next;
+                    }
+                    pre.next = null;
+                    crnt = pre;
+                }
+            }
+        }
+
+        public void remove(Node<E> p)
+        {
+            if (head != null)
+            {
+                if (p == head)
+                {
+                    removeFirst();
+                }
+                else
+                {
+                    Node<E> ptr = head;
+
+                    while (ptr.next != p)
+                    {
+                        ptr = ptr.next;
+                        if (ptr == null) return;
+                    }
+                    ptr.next = p.next;
+                    crnt = ptr;
+                }
+                
+            }
+        }
+
+        public void removeCurrentNode()
+        {
+            remove(crnt);
+        }
+
+        public void clear()
+        {
+            while (head != null)
+                removeFirst();
+            crnt = null;
+        }
+
+        public bool next()
+        {
+            if (crnt == null || crnt.next == null)
+                return false;
+            crnt = crnt.next;
+            return true;
+        }
+
+        public void printCurrentNode()
+        {
+            if (crnt == null)
+                Console.WriteLine("선택한 노드가 없습니다.");
+            else
+                Console.WriteLine(crnt.data);
+        }
+
+        public void dump()
+        {
+            Node<E> ptr = head;
+
+            while(ptr != null)
+            {
+                Console.WriteLine(ptr.data);
+                ptr = ptr.next;
+            }
+        }
+
+      
+    }
+}
---
+++ Chapter9/Chapter9/LinkedListTester.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    public class LinkedListTester
+    {
+
+
+        public class Data
+        {
+            static readonly int NO = 1;
+            static readonly int NAME = 2;
+
+            private int no;
+            private string name;
+
+            public string toString()
+            {
+                return "(" + no + ")" + name;
+            }
+
+            public void scanData(string guide, int sw)
+            {
+                Console.WriteLine(guide + "할 데이터를 입력하세요");
+
+                if ((sw & NO) == NO)
+                {
+                    Console.Write("번호 : ");
+                    no = Convert.ToInt32(Console.ReadLine());
+
+                }
+                if ((sw & NAME) == NAME)
+                {
+                    Console.Write("이름 : ");
+                    name = Console.ReadLine();
+                }
+            }
+
+            public readonly Comparer<Data> NO_ORDER = new NoOrderComparator();
+
+            private class NoOrderComparator : Comparer<Data>
+            {
+
+                public override int Compare(Data d1, Data d2)
+                {
+                    return d1.name.CompareTo(d2.name);
+                }
+            }
+
+        }
+
+        enum Menu
+        {
+
+            ADD_FIRST
+            
+
+
+        }
+
+
+
+
+
+
+
+        //static void Main(string[] args)
+        //    {
+        //    }
+    }
+}
